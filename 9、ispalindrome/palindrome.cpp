c9c67edfd7eb3a9dcb7d521a57694d630dcab509
root <root@DESKTOP-9A1D256.localdomain>
2025-02-16 23:25:51 +0800

    fix error

--- 9、ispalindrome/palindrome.cpp
+++ 9、ispalindrome/palindrome.cpp
@@ -6,15 +6,21 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x > 0 && x%10 != 0) {
+        if (x > 0) {
             vector<int> number;
-            int length = number.size();
             getNum(x, number);
-            for (int i = 0; i < length/2; i++) {
-                if (number[i] != number[length - i - 1]) return false;
+            int length = number.size();
+            for (int i = 0; i < length / 2; i++) {
+                if (number[i] == number[length - i - 1]) {
+                    continue;
+                } else 
+                    return false;
             }
+            if (x % 10 == 0) return false;
             return true;
         }
+        if (x == 0)
+            return true;
         return false;
     }
 
@@ -41,6 +47,10 @@ int main(){
     if (!flag) cout << "it is not palind rome" << endl;
 
     flag = solu.isPalindrome(10);
+    if (flag) cout << "it is palind rome" << endl;
+    if (!flag) cout << "it is not palind rome" << endl;
+
+    flag = solu.isPalindrome(123123);
     if (flag) cout << "it is palind rome" << endl;
     if (!flag) cout << "it is not palind rome" << endl;
 
